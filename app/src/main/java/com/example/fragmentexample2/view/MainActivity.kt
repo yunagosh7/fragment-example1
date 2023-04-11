b9683ecf56b915356feb2b108911a1b63811cc5d
Vila Juan Cruz <yunagosh7@gmail.com>
2023-04-11 00:03:57 -0300

    first commit

---
+++ app/src/main/java/com/example/fragmentexample2/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fragmentexample2.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fragmentexample2.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
